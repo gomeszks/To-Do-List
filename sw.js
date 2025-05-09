8ded5aed039a9361fe72cc9c9b2150897140356d
gomeszks <gomesluyza43@gmail.com>
2025-05-09 13:10:12 -0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+    caches.open('todo-cache').then(function (cache) {
+        return cache.addAll([
+        'index.html',
+        'style.css',
+        'script.js',
+        'manifest.json',
+        'nuvem.png',
+        'icon1.png'
+        ]);
+    })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+    caches.match(e.request).then(function (response) {
+        return response || fetch(e.request);
+    })
+    );
+});
